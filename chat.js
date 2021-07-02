33fbe858407fbc9f9f710079b5f9ef3e2d82ec7f
ShivanshVerma-coder <Shivansh29.sv@gmail.com>
2021-07-02 08:17:40 +0530

    ready for heroku

---
+++ chat.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const ourApp = express()
+const socketio = require("socket.io")
+var PORT = process.env.PORT || 3000
+ourApp.use(express.static(__dirname + "/Frontend"))
+
+const expressServer = ourApp.listen(PORT, () => {
+  console.log("server Started")
+})
+
+const io = socketio(expressServer)
+
+io.on("connect", (socket) => {
+  console.log(socket)
+  socket.on("dataToServer", (dataFromClient) => {
+    console.log(dataFromClient.data + " " + socket.id)
+  })
+  socket.on("chatMessageToServer", (data) => {
+    console.log(data.text + " " + data.id)
+    io.emit("chatMessageToAllClients", data)
+  })
+})
